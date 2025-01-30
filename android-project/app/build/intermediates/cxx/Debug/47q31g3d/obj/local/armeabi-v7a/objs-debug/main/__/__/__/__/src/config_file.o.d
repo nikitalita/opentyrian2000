9ddc910d00d93c405833ec43b34235f2f0e76299
Andy Vandijck <andyvand@gmail.com>
2025-01-30 10:43:17 +0100

    Fix for Android
    Fix for Android

--- android-project/app/build/intermediates/cxx/Debug/47q31g3d/obj/local/armeabi-v7a/objs-debug/main/__/__/__/__/src/config_file.o.d
+++
@@ -1,4 +0,0 @@
-/Users/andyvand/Downloads/opentyrian2000/android-project/app/build/intermediates/cxx/Debug/47q31g3d/obj/local/armeabi-v7a/objs-debug/main/__/__/__/__/src/config_file.o: \
-  /Users/andyvand/Downloads/opentyrian2000/android-project/app/jni/src/../../../../src/config_file.c \
-  /Users/andyvand/Downloads/opentyrian2000/android-project/app/jni/src/../../../../src/config_file.h
-/Users/andyvand/Downloads/opentyrian2000/android-project/app/jni/src/../../../../src/config_file.h:
